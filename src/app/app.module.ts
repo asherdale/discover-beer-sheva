0a2eb437dd712f2f4dd9b52488175d5959f0a199
Jake <jprusky99@gmail.com>
2018-01-21 17:08:17 +0200

    oh god there's no way this works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP } from '@ionic-native/http';
 
 
 import { MyApp } from './app.component';
@@ -46,6 +47,7 @@ import { StatusBar } from '@ionic-native/status-bar';
     StatusBar,
     SplashScreen,
     Geolocation,
+    HTTP,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
